f6f00fd54e58a84c8d44a2edf8af9653d6b48725
이지현 JiHyunLee <100523786+LeeJ1Hyun@users.noreply.github.com>
2023-11-07 20:35:27 +0900

    Create notify-slack.js

---
+++ .github/scripts/notify-slack.js
@@ -0,0 +1,36 @@
+const axios = require('axios');
+
+async function notifySlack() {
+  const githubToken = process.env.GITHUB_TOKEN;
+  const slackWebhookUrl = process.env.SLACK_WEBHOOK_URL;
+  const response = await axios.get('https://github.com/LeeJ1Hyun/noti-pr/pulls', {
+    headers: {
+      Authorization: `Bearer ${githubToken}`,
+    },
+    params: {
+      state: 'open',
+      sort: 'created',
+      direction: 'desc',
+    },
+  });
+
+  const prsToNotify = response.data.filter((pr) => {
+    const hasComments = pr.comments > 0;
+    const isApproved = pr.approved && pr.approved.some((review) => review.state === 'APPROVED');
+    const hasWipLabel = pr.labels.some((label) => label.name.toLowerCase() === 'wip');
+
+    return !hasComments && !isApproved && !hasWipLabel;
+  });
+
+  const prLinks = prsToNotify.map((pr) => pr.html_url);
+  if (prLinks.length > 0) {
+    const message = `Following PRs need attention:\n${prLinks.join('\n')}`;
+    await axios.post(slackWebhookUrl, { text: message });
+  } else {
+    console.log('No PRs to notify about.');
+  }
+}
+
+notifySlack().catch((error) => {
+  console.error('Error notifying Slack:', error);
+});
